68402800be0b14c56bb6b9663d86a4a750cac6eb
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2013-02-27 17:05:52 +0900

    bundle exec rails new APP_PATH -B -Tでアプリ作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MandarinOrange::Application.config.secret_key_base = '466dc8319f6b96becec8e69a2c2c6bb7fd40d10ba7b1239ead2aa854a6e49d33dd6231fe70e1366b39e5af36bb6b81c874b36a3cd1b3a3265e961b446cd25079'
